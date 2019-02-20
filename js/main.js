6bde82e71ab948bf2202a8c9bda332d8b6ff632f
Ivan Kirshak <i.kirshak@gmail.com>
2019-02-20 20:18:20 +0200

    mute to top button

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('.menu-button').on('click', () => {
 // TO TOP BUTTON IN CATALOGUE
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};*/
 
 /*function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
