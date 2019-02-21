17ceeb62a21e885240373d18061c2f0debe872bd
Ivan Kirshak <i.kirshak@gmail.com>
2019-02-21 15:43:47 +0200

    resturcturized javascript

---
+++ js/lazyLoad.js
@@ -0,0 +1,4 @@
+//lazyload iamges
+
+const observer = lozad();
+observer.observe();
--- js/toTopButton.js
+++ js/toTopButton.js
@@ -1,20 +1,7 @@
-//lazyload iamges
-
-/*const observer = lozad();
-observer.observe();*/
-
-//jquery menu
-
-$('.menu-button').on('click', () => {
-    $('.mobnav').toggle();
-  });
-
-
-
 // TO TOP BUTTON IN CATALOGUE
 
 // When the user scrolls down 20px from the top of the document, show the button
-/*window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -28,4 +15,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
-}*/
+}
